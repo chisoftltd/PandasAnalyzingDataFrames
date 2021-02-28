0c73abc75a83e6a56d78378fb3351e2249bbc62a
chisoftltd <chisoftltd@gmail.com>
2021-02-28 19:10:32 +0100

    Info About the Data

--- PandasAnalyzingDataFrames.py
+++ PandasAnalyzingDataFrames.py
@@ -4,6 +4,7 @@ import pandas as pd
 df = pd.read_csv('data.csv')
 
 print(df.head(10)) # Get a quick overview by printing the first 10 rows of the DataFrame
+print()
 
 # Print the first 5 rows of the DataFrame
 import pandas as pd
@@ -11,5 +12,11 @@ import pandas as pd
 df = pd.read_csv('data.csv')
 
 print(df.head())
+print()
 
 # Print the last 5 rows of the DataFrame
+print(df.tail())
+print()
+
+# Info About the Data
+print(df.info()) 
